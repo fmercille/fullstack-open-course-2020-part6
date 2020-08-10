82078d60180f6c2950d1e1082f7cb468e09b4b14
Frederic Mercille <fmercille@gmail.com>
2020-08-09 23:03:39 -0400

    Exercise 6.13

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -19,17 +10,18 @@ const asObject = (anecdote) => {
 
 const sorter = (a, b) => a.votes > b.votes ? -1 : (a.votes < b.votes ? 1 : 0)
 
-const initialState = anecdotesAtStart.map(asObject).sort(sorter)
-
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   let newState = state
 
   switch (action.type) {
-    case 'VOTE':
+    case 'ANECDOTE.VOTE':
       newState = state.map((anecdote) => anecdote.id === action.anecdote ? {...anecdote, votes: anecdote.votes + 1} : anecdote)
       break
-    case 'CREATE':
+    case 'ANECDOTE.CREATE':
       newState = [...state, asObject(action.anecdote)]
+      break
+    case 'ANECDOTE.INIT':
+      newState = action.data
       break
     default:
       break
@@ -40,15 +32,22 @@ const reducer = (state = initialState, action) => {
 
 const vote = (anecdote) => {
   return {
-    type: 'VOTE',
+    type: 'ANECDOTE.VOTE',
     anecdote: anecdote
   }
 }
 
 const createAnecdote = (anecdote) => {
   return {
-    type: 'CREATE',
+    type: 'ANECDOTE.CREATE',
     anecdote: anecdote
+  }
+}
+
+const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'ANECDOTE.INIT',
+    data: anecdotes
   }
 }
 
@@ -56,5 +55,6 @@ export default reducer
 
 export {
   vote,
-  createAnecdote
+  createAnecdote,
+  initializeAnecdotes
 }
