ada279eea14fd4499a42c7f5120285226d54393a
Frederic Mercille <fmercille@gmail.com>
2020-08-09 20:25:22 -0400

    Exercise 6.10

---
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,14 @@
+const initialState = 'Initial notification'
+
+const reducer = (state = initialState, action) => {
+  let newState = state
+
+  switch (action.type) {
+    default:
+      break
+  }
+
+  return newState
+}
+
+export default reducer
