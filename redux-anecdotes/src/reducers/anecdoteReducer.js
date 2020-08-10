3db0c9bfacd950fc8040a8800c19e6c8ee5ac40d
Frederic Mercille <fmercille@gmail.com>
2020-08-09 23:47:49 -0400

    Exercise 6.15

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,4 @@
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
+import anecdoteService from '../services/anecdotes'
 
 const sorter = (a, b) => a.votes > b.votes ? -1 : (a.votes < b.votes ? 1 : 0)
 
@@ -44,10 +36,13 @@ const createAnecdote = (data) => {
   }
 }
 
-const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: 'ANECDOTE.INIT',
-    data: anecdotes
+const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'ANECDOTE.INIT',
+      data: anecdotes
+    })
   }
 }
 
